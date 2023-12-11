684c67707bee1b63deec60ba470b1c5eb8da0b66
dinasoa <hei.dinasoa@gmail.com>
2023-12-11 13:44:38 +0300

    fix: quit when choose option 8

--- main.scala
+++ main.scala
@@ -1,56 +1,47 @@
 import scala.io.StdIn.readLine
 
-object Main extends App{
-  var quit = false
+object Main extends App {
+  val electricity = readLine("Is the electricity cut off? (1 for Yes, 2 for No)\n1- Yes\n2- No\n")
 
-  while (!quit) {
-    val electricity = readLine(
-      """Is the electricity cut off? (1 for Yes, 2 for No)
-        |1- Yes
-        |2- No
-        |""".stripMargin
-    )
-
-    electricity match {
-      case "1" =>
-        println("Mandreta afo na gaz fa tsy poinsa leh rice cooker vo tsisy jiro.")
-        quit = true
-      case "2" => userPrompt()
-      case _ => println("Choose a valid choice.")
-    }
+  if (electricity == "1") {
+    println("Mandreta afo na gaz fa tsy poinsa leh rice cooker vo tsisy jiro.")
+  } else if (electricity == "2") {
+    val riceCooker = new RiceCooker
+    userPrompt(riceCooker)
+  } else {
+    println("Choose a valid choice.")
   }
 
-  def userPrompt(): Unit = {
-    val riceCooker = new RiceCooker()
+  
+  def userPrompt(riceCooker: RiceCooker): Unit = {
     var userChoice = ""
 
-    while (userChoice != "8") {
-      userChoice = readLine(
-        """RICE COOKER:
-          |1- Plug in
-          |2- Unplug
-          |3- Turn on
-          |4- Turn off
-          |5- Add rice
-          |6- Add water
-          |7- Show status
-          |8- Quit
-          |""".stripMargin
-      )
+      do {
+        userChoice = readLine(
+          """RICE COOKER:
+            |1- Plug in
+            |2- Unplug
+            |3- Turn on
+            |4- Turn off
+            |5- Add rice
+            |6- Add water
+            |7- Show status
+            |8- Quit
+            |""".stripMargin
+        )
 
-      userChoice match {
-        case "1" => riceCooker.plug()
-        case "2" => riceCooker.unplug()
-        case "3" => riceCooker.turnOn()
-        case "4" => riceCooker.turnOff()
-        case "5" => riceCooker.addRice()
-        case "6" => riceCooker.addWater()
-        case "7" => riceCooker.getStatus()
-        case "8" =>
-        case _ => println("Invalid choice. Please enter a number between 1 and 8.")
-      }
-    }
+        if (userChoice != "8") {
+          userChoice match {
+            case "1" => riceCooker.plug()
+            case "2" => riceCooker.unplug()
+            case "3" => riceCooker.turnOn()
+            case "4" => riceCooker.turnOff()
+            case "5" => riceCooker.addRice()
+            case "6" => riceCooker.addWater()
+            case "7" => riceCooker.getStatus()
+            case _   => println("Invalid choice. Please enter a number between 1 and 8.")
+          }
+        }
+      } while (userChoice != "8")
   }
-
-  userPrompt()
 }
